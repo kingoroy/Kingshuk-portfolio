59a91bd885b3b8bc2dea1dc78d9ba659ca5c2b9b
kingofroy <kingshukr713@gmail.com>
2024-07-17 18:41:15 +0530

    scrolling change

--- src/App.js
+++ src/App.js
@@ -21,39 +21,30 @@ function App() {
   const personalProjectRef = useRef();
   const scrollTimeoutRef = useRef(null);
   const touchStartY = useRef(0);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const pageWidth = window.innerWidth;
+  const scrollThreshold = pageWidth / 2; // 50% of the page width
 
   const handleScroll = (deltaY) => {
     const newScrollY = scrollYRef.current + deltaY;
-    const clampedScrollY = Math.min(Math.max(newScrollY, 0), window.innerWidth * 4);
+    const clampedScrollY = Math.min(Math.max(newScrollY, 0), pageWidth * 4);
     setScrollY(clampedScrollY);
     scrollYRef.current = clampedScrollY;
+
+    // Check if scrollY has exceeded the scroll threshold
+    if (deltaY > 0 && clampedScrollY > (currentPage * pageWidth) + scrollThreshold) {
+      handleScrollToPage(currentPage + 1);
+    } else if (deltaY < 0 && clampedScrollY < (currentPage * pageWidth) - scrollThreshold) {
+      handleScrollToPage(currentPage - 1);
+    }
   };
 
   const handleScrollToPage = (page) => {
-    const scrollTo = page * window.innerWidth;
-    setScrollY(scrollTo);
-    scrollYRef.current = scrollTo;
-  };
-
-  const handleScrollEnd = () => {
-    let windowWidth = window.innerWidth;
-
-    if (scrollYRef.current >= windowWidth * 3.5) {
-      setScrollY(windowWidth * 4); // Scroll to PersonalProject
-      scrollYRef.current = windowWidth * 4;
-    } else if (scrollYRef.current >= windowWidth * 2.5) {
-      setScrollY(windowWidth * 3); // Scroll to Experience
-      scrollYRef.current = windowWidth * 3;
-    } else if (scrollYRef.current >= windowWidth * 1.5) {
-      setScrollY(windowWidth * 2); // Scroll to Skills
-      scrollYRef.current = windowWidth * 2;
-    } else if (scrollYRef.current >= windowWidth * 0.5) {
-      setScrollY(windowWidth); // Scroll to About
-      scrollYRef.current = windowWidth;
-    } else {
-      setScrollY(0); // Scroll to Home
-      scrollYRef.current = 0;
-    }
+    const clampedPage = Math.min(Math.max(page, 0), 4);
+    setScrollY(clampedPage * pageWidth);
+    setCurrentPage(clampedPage);
+    scrollYRef.current = clampedPage * pageWidth;
   };
 
   const onWheel = (e) => {
@@ -63,10 +54,6 @@ function App() {
     if (scrollTimeoutRef.current) {
       clearTimeout(scrollTimeoutRef.current);
     }
-
-    scrollTimeoutRef.current = setTimeout(() => {
-      handleScrollEnd();
-    }, 150);
   };
 
   const onTouchStart = (e) => {
@@ -83,10 +70,6 @@ function App() {
     if (scrollTimeoutRef.current) {
       clearTimeout(scrollTimeoutRef.current);
     }
-
-    // scrollTimeoutRef.current = setTimeout(() => {
-    //   handleScrollEnd();
-    // }, 150);
   };
 
   useEffect(() => {
@@ -102,7 +85,7 @@ function App() {
         clearTimeout(scrollTimeoutRef.current);
       }
     };
-  }, []);
+  }, [currentPage]);
 
   // Define animations for the pages
   const homePageProps = useSpring({
@@ -111,108 +94,109 @@ function App() {
   });
 
   const aboutPageProps = useSpring({
-    transform: `translateX(${Math.min(window.innerWidth, window.innerWidth - scrollY)}px)`,
+    transform: `translateX(${Math.min(pageWidth, pageWidth - scrollY)}px)`,
     config: { tension: 210, friction: 20 },
     opacity: scrollY > 0 ? 1 : 0,
     zIndex: scrollY > 0 ? 1 : 0,
   });
 
   const skillPageProps = useSpring({
-    transform: `translateX(${Math.min(2 * window.innerWidth - scrollY, window.innerWidth)}px)`,
+    transform: `translateX(${Math.min(2 * pageWidth - scrollY, pageWidth)}px)`,
     config: { tension: 210, friction: 20 },
-    opacity: scrollY > window.innerWidth ? 1 : 0,
-    zIndex: scrollY > window.innerWidth ? 1 : 0,
+    opacity: scrollY > pageWidth ? 1 : 0,
+    zIndex: scrollY > pageWidth ? 1 : 0,
   });
 
   const experiencePageProps = useSpring({
-    transform: `translateX(${Math.min(3 * window.innerWidth - scrollY, 2 * window.innerWidth)}px)`,
+    transform: `translateX(${Math.min(3 * pageWidth - scrollY, 2 * pageWidth)}px)`,
     config: { tension: 210, friction: 20 },
-    opacity: scrollY > window.innerWidth ? 2 : 0,
-    zIndex: scrollY > window.innerWidth ? 2 : 0,
+    opacity: scrollY > pageWidth * 2 ? 1 : 0,
+    zIndex: scrollY > pageWidth * 2 ? 1 : 0,
   });
 
   const personalProjectProps = useSpring({
-    transform: `translateX(${Math.min(4 * window.innerWidth - scrollY, 3 * window.innerWidth)}px)`,
+    transform: `translateX(${Math.min(4 * pageWidth - scrollY, 3 * pageWidth)}px)`,
     config: { tension: 210, friction: 20 },
-    opacity: scrollY > window.innerWidth ? 3 : 0,
-    zIndex: scrollY > window.innerWidth ? 3 : 0,
+    opacity: scrollY > pageWidth * 3 ? 1 : 0,
+    zIndex: scrollY > pageWidth * 3 ? 1 : 0,
   });
 
-  useEffect(()=>{
+  useEffect(() => {
     setTimeout(() => {
       setSplashScreenVisible(false);
     }, 2000);
-  })
+  }, []);
+
   return (
     <>
-    <Navbar handleScrollToPage={handleScrollToPage} splashScreenVisible={splashScreenVisible}/>
-      <SplashScreen  splashScreenVisible={splashScreenVisible}/> 
-    {!splashScreenVisible &&
-    <div style={{ overflow: 'hidden', position: 'relative', height: '100vh' }}>
-      <Contact />
-      <animated.div
-        style={{
-          ...homePageProps,
-          position: 'absolute',
-          width: '100%',
-          height: '100%',
-        }}
-        ref={homeRef}
-      >
-        <HomePage />
-      </animated.div>
-      <animated.div
-        style={{
-          ...aboutPageProps,
-          position: 'absolute',
-          width: '100%',
-          height: '100%',
-        }}
-        ref={aboutRef}
-      >
-        <Aboutme
-          windowWidth={window.innerWidth}
-          scrollY={scrollY}
-          scrollYRef={scrollYRef}
-        />
-      </animated.div>
-      <animated.div
-        style={{
-          ...skillPageProps,
-          position: 'absolute',
-          width: '100%',
-          height: '100%',
-        }}
-        ref={skillRef}
-      >
-        <Skills />
-      </animated.div>
-      <animated.div
-        style={{
-          ...experiencePageProps,
-          position: 'absolute',
-          width: '100%',
-          height: '100%',
-        }}
-        ref={experienceRef}
-      >
-        <Experience />
-      </animated.div>
-      <animated.div
-        style={{
-          ...personalProjectProps,
-          position: 'absolute',
-          width: '100%',
-          height: '100%',
-        }}
-        ref={personalProjectRef}
-      >
-        <PersonalProject />
-      </animated.div>
-      <Footer />
-    </div>}
+      <Navbar handleScrollToPage={handleScrollToPage} splashScreenVisible={splashScreenVisible} />
+      <SplashScreen splashScreenVisible={splashScreenVisible} />
+      {!splashScreenVisible && (
+        <div style={{ overflow: 'hidden', position: 'relative', height: '100vh' }}>
+          <Contact />
+          <animated.div
+            style={{
+              ...homePageProps,
+              position: 'absolute',
+              width: '100%',
+              height: '100%',
+            }}
+            ref={homeRef}
+          >
+            <HomePage />
+          </animated.div>
+          <animated.div
+            style={{
+              ...aboutPageProps,
+              position: 'absolute',
+              width: '100%',
+              height: '100%',
+            }}
+            ref={aboutRef}
+          >
+            <Aboutme
+              windowWidth={pageWidth}
+              scrollY={scrollY}
+              scrollYRef={scrollYRef}
+            />
+          </animated.div>
+          <animated.div
+            style={{
+              ...skillPageProps,
+              position: 'absolute',
+              width: '100%',
+              height: '100%',
+            }}
+            ref={skillRef}
+          >
+            <Skills />
+          </animated.div>
+          <animated.div
+            style={{
+              ...experiencePageProps,
+              position: 'absolute',
+              width: '100%',
+              height: '100%',
+            }}
+            ref={experienceRef}
+          >
+            <Experience />
+          </animated.div>
+          <animated.div
+            style={{
+              ...personalProjectProps,
+              position: 'absolute',
+              width: '100%',
+              height: '100%',
+            }}
+            ref={personalProjectRef}
+          >
+            <PersonalProject />
+          </animated.div>
+          <Footer />
+        </div>
+      )}
     </>
-
   );
 }
 
